5c3372dba309f4b4fe098b1e4436603013530cf4
Michael Cao <mcc249@cornell.edu>
2024-12-03 21:56:18 -0600

    Re-organize

--- 2024/Rust/src/day1.rs
+++ 2024/Rust/src/day1.rs
@@ -1,10 +1,8 @@
-use std::fs::File;
-use std::path::Path;
-use std::io::prelude::*;
 use std::vec::Vec;
 use regex::Regex;
 use std::iter::zip;
 use counter::Counter;
+use crate::helper::read_data;
 
 pub fn main() {
     
@@ -18,24 +16,6 @@ pub fn main() {
 
     println!("{p1}");
     println!("{p2}");
-
-}
-
-fn read_data(path_str: &str) -> String {
-
-    let mut file = match File::open(Path::new(&path_str)) {
-        Err(_) => panic!("File not found"),
-        Ok(file) => file,
-    };
-
-    let mut data = String::new();
-
-    match file.read_to_string(&mut data) {
-        Err(_) => panic!("Could not read"),
-        Ok(_) => (),
-    };
-
-    data
 
 }
 
