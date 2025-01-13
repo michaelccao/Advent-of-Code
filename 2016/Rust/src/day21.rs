1d6ec47be711b20b4e14cfe10be69684e4be53a7
Michael Cao <mcc249@cornell.edu>
2025-01-12 20:40:40 -0600

    2016 Day 21 Rust

--- 2016/Rust/src/day21.rs
+++ 2016/Rust/src/day21.rs
@@ -1,6 +1,6 @@
 use crate::helper::read_data;
-use std::vec::Vec;
 use itertools::Itertools;
+use std::vec::Vec;
 
 pub fn main() {
     let data: String = read_data("../Data/Day21.txt");
@@ -13,59 +13,60 @@ pub fn main() {
     // Too lazy to do this "properly", just brute force it LMAO
     for password in "abcdefgh".chars().permutations(8) {
         let password: String = password.iter().collect::<String>();
-        if scramble_password( &password[..], &instructions) == "fbgdceah".to_string() {
+        if scramble_password(&password[..], &instructions) == "fbgdceah".to_string() {
             println!("{password}");
         }
     }
-
 }
 
 fn scramble_password(start: &str, instructions: &Vec<&str>) -> String {
-
     let mut s: Vec<char> = start.chars().collect();
 
     for instruction in instructions {
         let instruction: Vec<&str> = instruction.split_whitespace().collect();
 
         match instruction[0] {
-            "rotate" => {rotate(&mut s, instruction)},
-            "swap" => {swap(&mut s, instruction)},
+            "rotate" => rotate(&mut s, instruction),
+            "swap" => swap(&mut s, instruction),
             "reverse" => {
                 let x: usize = instruction[2].parse().unwrap();
                 let y: usize = instruction[4].parse().unwrap();
 
-                for i in 0..(y-x+1)/2 {
-                    s.swap(x+i, y-i);
+                for i in 0..(y - x + 1) / 2 {
+                    s.swap(x + i, y - i);
                 }
-            },
+            }
             "move" => {
                 let x: usize = instruction[2].parse().unwrap();
                 let y: usize = instruction[5].parse().unwrap();
                 let removed = s.remove(x);
                 s.insert(y, removed);
-            },
+            }
             _ => {}
         }
     }
 
     s.iter().collect::<String>()
-
 }
 
 fn rotate(s: &mut Vec<char>, instruction: Vec<&str>) {
     let shift: usize = match instruction[1] {
         "right" => instruction[2].parse().unwrap(),
         "left" => s.len() - instruction[2].parse::<usize>().unwrap(),
-        "based" => { let ind = s.iter().position(|&x| x == instruction[6].chars().next().unwrap()).unwrap();
-            (1 + ind + if ind >= 4 {1} else {0}) % s.len() }, 
-        _ => 0
+        "based" => {
+            let ind = s
+                .iter()
+                .position(|&x| x == instruction[6].chars().next().unwrap())
+                .unwrap();
+            (1 + ind + if ind >= 4 { 1 } else { 0 }) % s.len()
+        }
+        _ => 0,
     };
 
     s.rotate_right(shift);
 }
 
 fn swap(s: &mut Vec<char>, instruction: Vec<&str>) {
-
     if instruction[1] == "letter" {
         let x = instruction[2].chars().next().unwrap();
         let y = instruction[5].chars().next().unwrap();
@@ -84,12 +85,10 @@ fn swap(s: &mut Vec<char>, instruction: Vec<&str>) {
         }
 
         s.swap(x_ind, y_ind);
-
     } else {
         let x_ind: usize = instruction[2].parse().unwrap();
         let y_ind: usize = instruction[5].parse().unwrap();
 
         s.swap(x_ind, y_ind);
     }
-
 }
