afab3b1fc8f9aa0a569b55f7706068305c19d2f0
Michael Cao <mcc249@cornell.edu>
2025-02-25 15:10:48 -0600

    2018 Rust Init

---
+++ 2018/Rust/src/helper.rs
@@ -0,0 +1,61 @@
+#![allow(dead_code)]
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+pub fn read_data(path_str: &str) -> String {
+    let mut file = match File::open(Path::new(&path_str)) {
+        Err(_) => panic!("File not found"),
+        Ok(file) => file,
+    };
+
+    let mut data = String::new();
+
+    match file.read_to_string(&mut data) {
+        Err(_) => panic!("Could not read"),
+        Ok(_) => (),
+    };
+
+    data
+}
+
+
+pub fn neighbors(i: usize, j: usize, rows: usize, cols: usize, diags: bool) -> Vec<(usize, usize)> {
+    let mut nbs: Vec<(usize, usize)> = Vec::new();
+
+    if i > 0 {
+        nbs.push((i - 1, j));
+    }
+
+    if j > 0 {
+        nbs.push((i, j - 1));
+    }
+
+    if i + 1 < rows {
+        nbs.push((i + 1, j));
+    }
+
+    if j + 1 < cols {
+        nbs.push((i, j + 1));
+    }
+
+    if diags {
+        if i > 0 && j > 0 {
+            nbs.push((i - 1, j - 1));
+        }
+
+        if i > 0 && j + 1 < cols {
+            nbs.push((i - 1, j + 1));
+        }
+
+        if i + 1 < rows && j > 0 {
+            nbs.push((i + 1, j - 1));
+        }
+
+        if i + 1 < rows && j + 1 < cols {
+            nbs.push((i + 1, j + 1));
+        }
+    }
+
+    nbs
+}
---
+++ 2018/Rust/src/main.rs
@@ -0,0 +1,6 @@
+mod helper;
+mod day25;
+
+fn main() {
+    day25::main();
+}
