0a674c02e8b8a000c8ed9ded71494be040c53192
Michael Cao <mcc249@cornell.edu>
2025-02-26 14:29:40 -0600

    Formatting

--- 2018/Rust/src/day05.rs
+++ 2018/Rust/src/day05.rs
@@ -8,22 +8,24 @@ pub fn main() {
     let p2: usize = super_simplify(&data);
 
     println!("{p1}\n{p2}");
-
 }
 
 fn simplify(s: &String, skip: Option<char>) -> usize {
     let mut s2: String = String::new();
 
     for c in s.chars() {
-        
         if let Some(skip_c) = skip {
-            if c.to_ascii_lowercase() == skip_c { continue }
+            if c.to_ascii_lowercase() == skip_c {
+                continue;
+            }
         }
 
         if s2.len() > 0 {
             let last_char: char = s2.pop().unwrap();
 
-            if last_char.to_ascii_lowercase() != c.to_ascii_lowercase() || last_char.is_lowercase() == c.is_lowercase() {
+            if last_char.to_ascii_lowercase() != c.to_ascii_lowercase()
+                || last_char.is_lowercase() == c.is_lowercase()
+            {
                 s2.push(last_char);
                 s2.push(c);
             }
@@ -35,9 +37,7 @@ fn simplify(s: &String, skip: Option<char>) -> usize {
     s2.len()
 }
 
-
 fn super_simplify(s: &String) -> usize {
-
     let mut shortest: usize = s.len();
     let a: u8 = 'a' as u8;
 
@@ -49,8 +49,6 @@ fn super_simplify(s: &String) -> usize {
         if l < shortest {
             shortest = l;
         }
-
-        
     }
 
     shortest
