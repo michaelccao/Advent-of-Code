ad8c4ffba0bfcfe1f5c103f289ad2c107fadf087
Michael Cao <michaelccao249@gmail.com>
2025-09-19 11:17:56 -0500

    2021 Day 15 Rust (Slow?)

---
+++ 2021/Rust/src/day15.rs
@@ -0,0 +1,81 @@
+use crate::helper::read_data;
+use std::collections::HashMap;
+
+pub fn main() {
+    let data: String = read_data("../Data/Day15.txt");
+    let grid: Vec<Vec<usize>> = data.lines().map(|line| line.chars().map(|c| c.to_digit(10).unwrap() as usize).collect()).collect();
+
+    let p1: usize = find_shortest_path(&grid);
+
+    println!("{p1}");
+
+    let p2: usize = find_shortest_path(&make_big_tile(&grid));
+
+    println!("{p2}");
+}
+
+fn find_shortest_path(grid: &Vec<Vec<usize>>) -> usize {
+
+    let mut nodes: Vec<(usize, usize, usize)> = vec![(0, 0, 0)];
+
+    let mut shortest: usize = usize::MAX;
+
+    let mut visited: HashMap<(usize, usize), usize> = HashMap::from([((0, 0), 0)]);
+
+    while nodes.len() > 0 {
+        let (i, j, cost) = nodes.pop().unwrap();
+
+        if i == grid.len() - 1 && j == grid[i].len() - 1 {
+            shortest = shortest.min(cost);
+        }
+
+        if cost + grid.len() - i - 1 + grid[i].len() - j - 1 >= shortest {
+            continue;
+        }
+
+        let neighbors: [(usize, usize); 4] = [
+            (i-1, j),
+            (i, j-1),
+            (i+1, j),
+            (i, j+1),
+        ];
+
+        for (i2, j2) in neighbors {
+            if i2 < grid.len() && j2 < grid.len() {
+                let cost2 = cost + grid[i2][j2];
+                if let Some(&visited_cost) = visited.get(&(i2, j2)) {
+                    if cost2 < visited_cost {
+                        visited.insert((i2, j2), cost2);
+                        nodes.push((i2, j2, cost2));
+                    }
+                } else {
+                    visited.insert((i2, j2), cost2);
+                    nodes.push((i2, j2, cost2));
+                }
+            }
+        }
+    }
+
+    shortest
+}
+
+fn make_big_tile(grid: &Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+
+    let mut grid2: Vec<Vec<usize>> = vec![vec![0;grid[0].len()*5];grid.len()*5];
+
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            for tile_i in 0..5 {
+                for tile_j in 0..5 {
+                    let mut cost: usize = grid[i][j] + tile_i + tile_j;
+                    while cost > 9 {
+                        cost -= 9;
+                    }
+                    grid2[tile_i*grid.len() + i][tile_j*grid[i].len() + j] = cost;
+                }
+            }
+        }
+    }
+
+    grid2
+}
